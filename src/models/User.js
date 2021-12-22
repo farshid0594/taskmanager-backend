a4b6d7c658d5794cdad2ded35abf20434b94f3d8
Mohammad <48600357+MohammadKoolivand@users.noreply.github.com>
2021-12-23 01:38:58 +0330

    resolve reviews for user model

--- src/models/User.js
+++ src/models/User.js
@@ -1,21 +1,20 @@
-
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
-    {
-        username: { type: String, required: true },
-        password: { type: String, required: true },
-        mobile: { type: String, required: true },
-        name: String,
-        tokens: [{ text: String, expired: String }],
-        code: { codeNo: Number, expired: String },
-        isActive: { type: Boolean, default: false },
-    },
-    {
-        timestamps: true,
-    }
+  {
+    username: { type: String, required: true, unique: true, index: true },
+    password: { type: String, required: true },
+    mobile: { type: String, required: true, unique: true },
+    name: { type: String, maxlength: 30 },
+    tokens: [{ text: String, expired: String }],
+    code: { text: Number, expired: String },
+    isActive: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const User = mongoose.model("User", UserSchema);
