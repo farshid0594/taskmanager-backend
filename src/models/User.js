506ac1edff8072e7620739638eefec3a912d6e92
Mohammad <48600357+MohammadKoolivand@users.noreply.github.com>
2021-12-24 13:57:50 +0330

    change-model-index mobile

--- src/models/User.js
+++ src/models/User.js
@@ -6,7 +6,7 @@ const UserSchema = new Schema(
   {
     username: { type: String, unique: true, index: true },
     password: { type: String },
-    mobile: { type: String, required: true, unique: true },
+    mobile: { type: String, required: true, unique: true, index:true },
     name: { type: String, maxlength: 30 },
     tokens: [{ text: String, expired: String }],
     code: { text: String, expired: String },
